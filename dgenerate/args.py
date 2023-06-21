5a35a097b82472d77cf005c78fff52736ce05d33
Teriks <https://github.com/Teriks/dgenerate>
2023-06-20 22:02:36 -0500

    Fix disrespect of --output-size when not using image seeds

--- dgenerate/args.py
+++ dgenerate/args.py
@@ -244,7 +244,7 @@ def parse_args():
     if args.gen_seeds is not None:
         args.seeds = args.gen_seeds
 
-    if len(args.image_seeds) == 0 and args.output_size is None:
+    if args.output_size is None and len(args.image_seeds) == 0:
         args.output_size = (512, 512)
 
     return args
