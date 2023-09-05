96773a9dc80f0cc2aec486dc40c1161896f43268
Teriks <Teriks@users.noreply.github.com>
2023-09-04 23:25:08 -0500

    Remove debug print

--- dgenerate/pipelinewrappers.py
+++ dgenerate/pipelinewrappers.py
@@ -268,7 +268,6 @@ def _create_torch_diffusion_pipeline(pipeline_type,
             kwargs.update(extra_args)
 
         if _is_single_file_model_load(model_path):
-            print(model_path)
             if model_subfolder is not None:
                 raise NotImplementedError('Single file model loads do not support the subfolder option.')
             pipeline = pipeline_class.from_single_file(model_path,
