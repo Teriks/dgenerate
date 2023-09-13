07caca74599098721c3b07ec1a5cccaf78bd5b83
Teriks <Teriks@users.noreply.github.com>
2023-09-12 19:02:09 -0500

    Delete wix intermediate files before building installer

--- installer/build.bat
+++ installer/build.bat
@@ -3,6 +3,7 @@ set PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin
 rmdir build /s /q
 rmdir dist /s /q
 rmdir wixobj /s /q
+del /s DgenerateComponents.wxs
 
 pyinstaller dgenerate.spec --clean
 heat dir dist\dgenerate -o DgenerateComponents.wxs -scom -frag -srd -sreg -gg -cg DgenerateComponents -dr INSTALLFOLDER
