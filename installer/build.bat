f58ab2d6cb1ae2c1ad78b4bb230c8cb828252643
Teriks <Teriks@users.noreply.github.com>
2023-09-12 21:50:37 -0500

    Zip up installer when done building

--- installer/build.bat
+++ installer/build.bat
@@ -1,5 +1,5 @@
-set PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin
 
+set PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin
 rmdir build /s /q
 rmdir dist /s /q
 rmdir wixobj /s /q
@@ -9,3 +9,4 @@ pyinstaller dgenerate.spec --clean
 heat dir dist\dgenerate -o DgenerateComponents.wxs -scom -frag -srd -sreg -gg -cg DgenerateComponents -dr INSTALLFOLDER
 candle Product.wix DgenerateComponents.wxs -arch x64 -out wixobj\ -ext WixUIExtension
 light -b dist\dgenerate wixobj\Product.wixobj wixobj\DgenerateComponents.wixobj -out wixobj\dgenerate.msi -ext WixUIExtension
+tar -a -c -f dgenerate_installer.zip -C wixobj dgenerate.msi cab1.cab cab2.cab cab3.cab
