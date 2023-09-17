8f8c4c49f45ac3b32be765e38ea1c58b113b4498
Teriks <Teriks@users.noreply.github.com>
2023-09-17 06:53:16 -0500

    Update installer build to auto create multipart archive

--- installer/build.bat
+++ installer/build.bat
@@ -9,4 +9,6 @@ pyinstaller dgenerate.spec --clean
 heat dir dist\dgenerate -o DgenerateComponents.wxs -scom -frag -srd -sreg -gg -cg DgenerateComponents -dr INSTALLFOLDER
 candle Product.wix DgenerateComponents.wxs -arch x64 -out wixobj\ -ext WixUIExtension
 light -b dist\dgenerate wixobj\Product.wixobj wixobj\DgenerateComponents.wixobj -out wixobj\dgenerate.msi -ext WixUIExtension
-tar -a -c -f dgenerate_installer.zip -C wixobj dgenerate.msi cab1.cab cab2.cab cab3.cab
+
+cd /D wixobj
+"C:\Program Files\7-Zip\7z.exe" -v1500m a dgenerate_installer.zip dgenerate.msi cab1.cab cab2.cab cab3.cab
