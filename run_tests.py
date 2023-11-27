f63e587c3b1b740ccb4833acc41e8f3b92d834e5
Teriks <Teriks@users.noreply.github.com>
2023-11-26 19:34:08 -0600

    unit tests

--- run_tests.py
+++ run_tests.py
@@ -6,6 +6,6 @@ runner = unittest.TextTestRunner()
 if runner.run(unittest.defaultTestLoader.discover("tests", pattern='*_test.py')).wasSuccessful():
     print('unit tests passed, running examples..')
     subprocess.run(
-        'python examples/run.py --device cuda:1 --offline-mode --short-animations --output-configs --output-metadata -v > examples/examples.log 2>&1', shell=True)
+        'python examples/run.py --device cuda:1 --short-animations --output-configs --output-metadata -v > examples/examples.log 2>&1', shell=True)
 else:
     exit(1)
