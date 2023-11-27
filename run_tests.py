f2819b05ecb1faccbf06ebb25bc649a8aee4f160
Teriks <Teriks@users.noreply.github.com>
2023-11-27 16:27:07 -0600

    better test script

--- run_tests.py
+++ run_tests.py
@@ -1,11 +1,28 @@
-import unittest
+import argparse
+import os
 import subprocess
+import unittest
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-d', '--device', default='cuda')
+parser.add_argument('-c', '--clean', default=False, action='store_true')
+
+args = parser.parse_args()
 
 runner = unittest.TextTestRunner()
 
 if runner.run(unittest.defaultTestLoader.discover("tests", pattern='*_test.py')).wasSuccessful():
     print('unit tests passed, running examples..')
-    subprocess.run(
-        'python examples/run.py --device cuda:1 --short-animations --output-configs --output-metadata -v > examples/examples.log 2>&1', shell=True)
+
+    if args.clean:
+        os.chdir('examples')
+        print('running: git clean -f -d in examples folder...')
+        subprocess.run('git clean -f -d', shell=True)
+        os.chdir('..')
+
+    run_string = f'python examples/run.py --device {args.device} --short-animations --output-configs --output-metadata -v > examples/examples.log 2>&1'
+    print('running:', run_string)
+    subprocess.run(run_string, shell=True)
 else:
     exit(1)
