42aa84fec8240bdbf34205819e45620840daaeaa
Teriks <Teriks@users.noreply.github.com>
2023-11-26 19:33:40 -0600

    unit tests

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,11 @@
 import unittest
+import subprocess
 
 runner = unittest.TextTestRunner()
-exit(not runner.run(unittest.defaultTestLoader.discover("tests", pattern='*_test.py')).wasSuccessful())
+
+if runner.run(unittest.defaultTestLoader.discover("tests", pattern='*_test.py')).wasSuccessful():
+    print('unit tests passed, running examples..')
+    subprocess.run(
+        'python examples/run.py --device cuda:1 --offline-mode --short-animations --output-configs --output-metadata -v > examples/examples.log 2>&1', shell=True)
+else:
+    exit(1)
