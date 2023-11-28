30b9b127276ae453b0b5699921316ebc1907ea83
Teriks <Teriks@users.noreply.github.com>
2023-11-28 15:45:50 -0600

    test offline mode

--- run_tests.py
+++ run_tests.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--device', default='cuda')
 parser.add_argument('-c', '--clean', default=False, action='store_true')
 parser.add_argument('-e', '--examples', default=False, action='store_true')
+parser.add_argument('-o', '--offline-mode', default=False, action='store_true')
 
 args = parser.parse_args()
 
@@ -26,7 +27,8 @@ if runner.run(unittest.defaultTestLoader.discover("tests", pattern='*_test.py'))
         subprocess.run('git clean -f -d', shell=True)
         os.chdir('..')
 
-    run_string = f'python examples/run.py --device {args.device} --short-animations --output-configs --output-metadata -v > examples/examples.log 2>&1'
+    offline = ' --offline-mode' if args.offline_mode else ''
+    run_string = f'python examples/run.py --device {args.device} --short-animations --output-configs --output-metadata{offline} -v > examples/examples.log 2>&1'
     print('running:', run_string)
     subprocess.run(run_string, shell=True)
 else:
