cd5a1c3fa7fc22bf97f2e51ab2a06509cd2d7430
Teriks <Teriks@users.noreply.github.com>
2024-06-16 14:11:33 -0500

    highlighting

--- dgenerate/pygments.py
+++ dgenerate/pygments.py
@@ -145,7 +145,7 @@ _VAE_KEYWORDS = sorted((
 _MODEL_TYPE_KEYWORDS = sorted((
     'torch',
     'torch-pix2pix',
-    'torch-sd3'
+    'torch-sd3',
     'torch-sdxl',
     'torch-if',
     'torch-ifs',
