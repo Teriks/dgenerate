f9d3f0eabca0b04ed40da111aa426e00bc0573a9
Teriks <Teriks@users.noreply.github.com>
2024-07-19 15:52:22 -0500

    enable ncnn in docker test environment

--- docker/install_dev.sh
+++ docker/install_dev.sh
@@ -1,5 +1,5 @@
 export DGENERATE_POETRY_LOCKFILE_PATH="$(pwd)/poetry/poetry.lock"
 export DGENERATE_POETRY_PYPROJECT_PATH="$(pwd)/poetry/pyproject.toml"
 
-pip3 install --editable .[flax] --extra-index-url https://download.pytorch.org/whl/cu121/ \
+pip3 install --editable .[flax, ncnn] --extra-index-url https://download.pytorch.org/whl/cu121/ \
 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
