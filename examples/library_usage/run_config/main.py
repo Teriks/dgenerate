334e796783329dae591c70b93cdbdc058888b27b
Teriks <Teriks@users.noreply.github.com>
2024-11-05 20:35:09 -0600

    patch bump

--- examples/library_usage/run_config/main.py
+++ examples/library_usage/run_config/main.py
@@ -6,7 +6,7 @@ import dgenerate.batchprocess
 
 
 config = r"""
-#! dgenerate 4.3.3
+#! dgenerate 4.3.4
 
 stabilityai/stable-diffusion-2 --prompts "a man walking on the moon without a space suit"
 
