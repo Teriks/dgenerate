ea1d4384502efab50a2c9d2b82bcfebe7b6b8713
Teriks <Teriks@users.noreply.github.com>
2025-01-15 00:25:15 -0600

    make-dev-env.sh, ROCm aware

--- make-dev-env.sh
+++ make-dev-env.sh
@@ -6,4 +6,8 @@ python3 -m venv venv
 
 source venv/bin/activate
 
-pip3 install --editable "$(dirname "$0")[dev,ncnn]" --extra-index-url https://download.pytorch.org/whl/cu124/
+if which rocminfo &> /dev/null; then
+  pip3 install --editable "$(dirname "$0")[dev,ncnn]" --extra-index-url https://download.pytorch.org/whl/rocm6.2/
+else
+  pip3 install --editable "$(dirname "$0")[dev,ncnn]" --extra-index-url https://download.pytorch.org/whl/cu124/
+fi
