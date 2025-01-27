a0d230fa9382041795f442bfcfa0a6a845e237d8
Teriks <Teriks@users.noreply.github.com>
2025-01-26 21:17:18 -0600

    make documentation more readable on desktop

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+html_css_files = [
+    'css/custom.css',
+]
+
 autodoc_member_order = 'groupwise'
 
 html_theme_options = {'navigation_depth': 4}
