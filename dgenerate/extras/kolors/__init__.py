9f0045a2080e4e378143b86625be817da9369f68
Teriks <Teriks@users.noreply.github.com>
2025-01-29 06:12:41 -0600

    implement Kolors pipelines not in diffusers #11
    heavily modify original kolors code to add pipelines
    currently not supported by diffusers, inpaint, and
    (img2img + controlnet)
    
    synthesize currently non-existant (txt2img + controlnet) and
    (inpaint + controlnet) pipelines for completeness
    
    add support for cpu offloading (vram optimizations)

---
+++ dgenerate/extras/kolors/__init__.py
@@ -0,0 +1,4 @@
+from dgenerate.extras.kolors.pipelines.pipeline_kolors_inpainting import KolorsInpaintPipeline
+from dgenerate.extras.kolors.pipelines.pipeline_controlnet_xl_kolors_img2img import KolorsControlNetImg2ImgPipeline
+from dgenerate.extras.kolors.pipelines.pipeline_controlnet_xl_kolors import KolorsControlNetPipeline
+from dgenerate.extras.kolors.pipelines.pipeline_controlnet_xl_kolors_inpaint import KolorsControlNetInpaintPipeline
